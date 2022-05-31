7b35291388b01d3c8a8b14e16e9d3d47810d43f9
Yoo dong hyeok <robin14dev@gmail.com>
2022-05-31 21:21:24 +0900

    merge modal & router

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navigationbar from "./Navigationbar";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ isLogin, loginHandler }) => {
   return (
     <div className="Header">
       <Link to="/">
         <img alt="로고"></img>
       </Link>
-      <Navigationbar />
+      <h1>Header component</h1>
+      <Navigationbar loginHandler={loginHandler} isLogin={isLogin} />
     </div>
   );
 };
