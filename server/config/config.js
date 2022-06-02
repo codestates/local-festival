22ebff17f4272842a4767f6fcd45c52c7b493bde
hongsuwon <dmswllalala5@gmail.com>
2022-06-02 16:26:11 +0900

    config localhost 변경

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const config = {
   development: {
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: process.env.DATABASE_SPRINT_PASSWORD,
     database: "local_festival",
-    connectTimeout : 60000
+    connectTimeout: 60000,
   },
 };
 
