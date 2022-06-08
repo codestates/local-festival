60ba1c7bda9b636b2cc90c2f43932c4adde3b82f
hongsuwon <dmswllalala5@gmail.com>
2022-06-08 15:03:28 +0900

    config.js deploy 추가

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv');
-const config = require('../config/config');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const config = require("../config/config");
 dotenv.config();
 
 const con = mysql.createConnection(
-  config[process.env.NODE_ENV || 'development']
+  // 여기도 수정
+  config[process.env.NODE_ENV || "deploy"]
 );
 
 con.connect((err) => {
